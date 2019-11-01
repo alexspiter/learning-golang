6b5d9af81a6791a4733f14bca05acf1562146ed1
Самкова Анна Алексеевна <anya.dho@gmail.com>
2019-11-02 00:40:49 +0500

    1. Сумма трехзначного числа

--- 1. Сумма и произведение цифр трёхзначного числа/main.go
+++ 1. Сумма и произведение цифр трёхзначного числа/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "../base/rand"
-import "../base/console"
+import (
+	"awesomeProject/base/console"
+	"awesomeProject/base/rand"
+	"fmt"
+)
 
 func main() {
-	console.Writeln(rand.RandomInt(10, 15))
+	console.Writeln(rand.RandomInt(100, 999))
+
+	var generatedNumber, ones, tens, hundreds int
+	var sum, multiplication int
+
+	generatedNumber = rand.RandomInt(100, 999)
+	ones = generatedNumber % 10
+	tens = generatedNumber % 100 / 10
+	hundreds = generatedNumber % 1000 / 100
+	fmt.Println(generatedNumber, ones, tens, hundreds)
+
+	sum = ones + tens + hundreds
+	multiplication = ones * tens * hundreds
+
+	fmt.Println("The random generated number is: ", generatedNumber)
+	fmt.Println("The sum of numerals is: ", sum)
+	fmt.Println("The multiplication is: ", multiplication)
 }
