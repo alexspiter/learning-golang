0fb30614e6c4a316a24a7dbdece1da46dcdbd08b
Максунов Артем Дмитриевич <maksunov@skbkontur.ru>
2019-11-01 23:17:58 +0500

    package rand

---
+++ base/rand/main.go
@@ -0,0 +1,14 @@
+package rand
+
+import (
+	"math/rand"
+	"time"
+)
+
+func RandomInt(min int, max int) int {
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+
+	return random.Intn(max+1-min) + min
+
+}
