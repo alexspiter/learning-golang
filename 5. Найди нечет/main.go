250550e2fea1d22e4f7e058d75192214297615ab
Самкова Анна Алексеевна <anya.dho@gmail.com>
2019-11-20 19:06:31 +0500

    Readme: ссылка на четвертую задачу

--- 5. Найди нечет/main.go
+++ 5. Найди нечет/main.go
@@ -1,1 +1,18 @@
-package ___Найди_нечет
+package main
+
+import "../base/console"
+
+func main() {
+	var first, second int
+
+	console.Writeln("Введите произвольные четное и нечетное числа")
+	first = console.ReadInt("Введите первое число: ")
+	second = console.ReadInt("Введите второе число: ")
+
+	if first%2 != 0 {
+		console.Writeln(first, "is an odd number.")
+		return
+	} else {
+		console.Writeln(second, "is an odd number.")
+	}
+}
