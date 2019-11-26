465e45d3376eff9349ed0c08c53ffcd0bf84d8d9
Самкова Анна Алексеевна <anya.dho@gmail.com>
2019-11-26 23:43:09 +0500

    Readme: ссылка на четвертую задачу

---
+++ 13. Выбор фигуры/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "../base/console"
+
+func main() {
+	var x int
+	console.Writeln("Введите номер фигуры (1 - квадрат, 2 - прямоугольник, 3 - треугольник, 4 - круг")
+	x = console.ReadInt()
+	switch x {
+	case 1:
+		console.Writeln("квадрат")
+	case 2:
+		console.Writeln("прямоугольник")
+	case 3:
+		console.Writeln("треугольник")
+	case 4:
+		console.Writeln("круг")
+	default:
+		console.Writeln("Значение переменной не определено.")
+	}
+
+}
