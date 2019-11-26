16bd217d6e6d1fc6eb1bcde96a537301a7817e88
Самкова Анна Алексеевна <anya.dho@gmail.com>
2019-11-26 22:13:58 +0500

    Readme: ссылка на четвертую задачу

---
+++ 12. Четверть на координатной плоскости/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "../base/console"
+
+func main() {
+
+	var x, y int
+
+	console.Writeln("Введите координаты точки")
+	x = console.ReadInt("Введите x: ")
+	y = console.ReadInt("Введите y: ")
+
+	if x > 0 && y > 0 {
+		console.Writeln("Точка с заданными координатами находится в I четверти.")
+		return
+	}
+
+	if x > 0 && y < 0 {
+		console.Writeln("Точка с заданными координатами находится во II четверти.")
+		return
+	}
+
+	if x < 0 && y < 0 {
+		console.Writeln("Точка с заданными координатами находится в III четверти.")
+		return
+	}
+	if x < 0 && y > 0 {
+		console.Writeln("Точка с заданными координатами находится в VI четверти.")
+	}
+	if x == 0 && y != 0 || y == 0 && x != 0 {
+		console.Writeln("Точка с заданными координатами находится на оси координат.")
+	} else {
+		console.Writeln("Точка с координатами [0, 0] находится в центре координатной плоскости.")
+	}
+}
