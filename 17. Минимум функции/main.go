1f87501de6e7105b5b516234b71cf6f85c5df3bc
Самкова Анна Алексеевна <anya.dho@gmail.com>
2020-02-19 20:40:56 +0500

    4. Случайное трёхзначное число, кратное десяти

--- 17. Минимум функции/main.go
+++ 17. Минимум функции/main.go
@@ -3,14 +3,13 @@ package main
 import "../base/console"
 
 func main() {
-	var a, b, doubleA, x float64
+	var a, b, x float64
 
 	a = console.ReadFloat("Введите значение a: ")
 	b = console.ReadFloat("Введите значение b: ")
 	console.ReadInt("Введите значение с: ")
 
-	doubleA = 2 * a
-	x = -b / doubleA
+	x = -b / 2 * a
 
 	if a < 0 {
 		console.Writeln("При a<0, ветви функции направлены вниз, т.е. x - максимум.")
@@ -20,8 +19,6 @@ func main() {
 		console.Writeln("При а = 0 решение невозможно")
 		return
 	}
-	if a > 0 {
-		console.Writeln("Минимум функции равен: ", x)
-	}
+	console.Writeln("Минимум функции равен: ", x)
 
 }
