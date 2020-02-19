f31f033e266d2589698f58f8ae44c20517938b7f
Самкова Анна Алексеевна <anya.dho@gmail.com>
2020-02-19 20:56:04 +0500

    4. Случайное трёхзначное число, кратное десяти

--- 15. Площади геометрических фигур/main.go
+++ 15. Площади геометрических фигур/main.go
@@ -5,31 +5,31 @@ import (
 	"math"
 )
 
+const Square = 1
+const Rectangle = 2
+const Triangle = 3
+const Circle = 4
+
 func main() {
-	var x int
-	var r, side1, side2, sSquare, sTriangle, sCircle float64
 
-	x = console.ReadInt("Введите номер фигуры: 1 - квадрат, 2 - прямоугольник, 3 - треугольник, 4 - круг\n")
+	var figureNumber int
+	figureNumber = console.ReadInt("Введите номер фигуры: 1 - квадрат, 2 - прямоугольник, 3 - треугольник, 4 - круг\n")
 
-	switch x {
-	case 1:
-		side1 = console.ReadFloat("Введите длину ")
-		sSquare = side1 * side1
-		console.Writeln("Площадь квадрата равна ", sSquare)
-	case 2:
-		side1 = console.ReadFloat("Введите длину ")
-		side2 = console.ReadFloat("Введите ширину ")
-		sSquare = side1 * side2
-		console.Writeln("Площадь прямоугольника равна ", sSquare)
-	case 3:
-		side1 = console.ReadFloat("Введите высоту ")
-		side2 = console.ReadFloat("Введите длину основания ")
-		sTriangle = 0.5 * side1 * side2
-		console.Writeln("Площадь треугольника равна", sTriangle)
-	case 4:
-		r = console.ReadFloat("Введите радиус круга ")
-		sCircle = math.Pi * (r * r)
-		console.Writeln("Площадь круга равна", sCircle)
+	switch figureNumber {
+	case Square:
+		side1 := console.ReadFloat("Введите длину ")
+		console.Writeln("Площадь квадрата равна ", side1*side1)
+	case Rectangle:
+		side1 := console.ReadFloat("Введите длину ")
+		side2 := console.ReadFloat("Введите ширину ")
+		console.Writeln("Площадь прямоугольника равна ", side1*side2)
+	case Triangle:
+		side1 := console.ReadFloat("Введите высоту ")
+		side2 := console.ReadFloat("Введите длину основания ")
+		console.Writeln("Площадь треугольника равна", 0.5*side1*side2)
+	case Circle:
+		r := console.ReadFloat("Введите радиус круга ")
+		console.Writeln("Площадь круга равна", math.Pi*(r*r))
 	default:
 		console.Writeln("Значение переменной не определено.")
 	}
